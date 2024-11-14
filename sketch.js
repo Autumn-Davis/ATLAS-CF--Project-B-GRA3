c9c77603c8a887fe5d02bd96f440f2e86e80a808
Autumn-Davis <autumn.davis@colorado.edu>
2024-11-14 14:32:24 -0700

    Snowman in class test

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() 
+{
+	createCanvas(400, 400);
+}
+
+function draw()
+{
+    background (220);
+    stroke('white');
+    ellipse(width/2, height * 0.8, 100, 120);
+    ellipse(width/2, height * 0.6, 80, 70);
+}
