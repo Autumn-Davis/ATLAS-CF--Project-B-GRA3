a0f145aefad4ccd54954ce36cae679697494dceb
Annie Bronson <anbr1175@colorado.edu>
2024-11-14 15:15:30 -0700

    Changed the color

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup()
 function draw()
 {
     background (220);
-    stroke('white');
+    stroke('blue');
     ellipse(width/2, height * 0.8, 100, 120);
     ellipse(width/2, height * 0.6, 80, 70);
 }
