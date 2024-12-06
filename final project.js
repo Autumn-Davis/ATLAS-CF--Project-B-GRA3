00fccf962864bd333e1234cf065715be58fb2eee
Autumn-Davis <autumn.davis@colorado.edu>
2024-12-06 14:53:23 -0700

    farmer game things

--- final project.js
+++ final project.js
@@ -1,3 +1,9 @@
+//make each flower a class, so that if they make contact with a farmer they stop decaying, then make a flower count to set up the win condition
+//Set up a game manager/scene manager through a class
+
+
+
+
 let c = ("#a34749")
 let sk = ("#dec58f")
 let farmerx = 0
@@ -5,10 +11,8 @@ let farmery = 0
 
 function setup() {
   createCanvas(800, 800);
-  
-
-  
 }
+
 
 function keyPressed () {
   if (keyCode === DOWN_ARROW && farmery < 6){
@@ -30,92 +34,61 @@ if (keyCode === LEFT_ARROW && farmerx > 0){
 }
 
 
-
-
-
-
 function draw() {
-  
-//grass
-  background("#5a6c2b");
-  fill("#8bae4f");
-  noStroke();
-
-  square(0,0,100,0);
-  square(100,100,100);
-  square(200,200,100);
-  square(300,300,100);
-  square(0,200,100);
-  square(100,300,100);
-  square(200,0,100);
-  square(300,100,100);
-
-  square(0+400,0,100,0);
-  square(100+400,100,100);
-  square(200+400,200,100);
-  square(300+400,300,100);
-  square(0+400,200,100);
-  square(100+400,300,100);
-  square(200+400,0,100);
-  square(300+400,100,100);
-
-  square(0,0+400,100,0);
-  square(100,100+400,100);
-  square(200,200+400,100);
-  square(300,300+400,100);
-  square(0,200+400,100);
-  square(100,300+400,100);
-  square(200,0+400,100);
-  square(300,100+400,100);
-  
-  square(0+400,0+400,100,0);
-  square(100+400,100+400,100);
-  square(200+400,200+400,100);
-  square(300+400,300+400,100);
-  square(0+400,200+400,100);
-  square(100+400,300+400,100);
-  square(200+400,0+400,100);
-  square(300+400,100+400,100);
-  
-  //flower
-  
-//   fill(c);
-//   ellipse(50,30,10,20);
-//   ellipse(50,60,10,20);
-//   ellipse(35,45,20,10);
-//   ellipse(65,45,20,10);
-//   fill("brown");
-//   circle(50,45,15);
-  
-  DrawFarmer()  
+    background(220);
+    drawBackground();
+    drawFarmer();  
 }
 
 
-function DrawFarmer () {
-push();
-  translate( farmerx * 100, farmery * 100)
+function drawBackground(){
+    let offset = width / 4;
+    for (let i = 0; i < 8; i++) {
+      let x = i * offset;
+      for (let j = 0; j < 8; j++) {
+        let y = j * offset / 2;
+        let stagger = 0;
+        if (j % 2 == 1) {
+          stagger = offset / 2;
+        }
+        drawSquare(x,y,stagger);
+      }
+    }
+  }  
   
-  stroke(1)
-  fill(sk);
-  //halment of farmer
-  let halmet = circle(50,150,25);
+  
+function drawSquare(x,y,stagger){
+    noStroke();
+    fill("#8bae4f");
+    square(x + stagger, y, 100, 10);
+  }
+
+
+farmer.drawFarmer
+    function drawFarmer() {
+        push();
+            translate( farmerx * 100, farmery * 100)
+  
+        strokeWeight(1);
+        fill(sk);
+            //farmer hat
+            let hat = circle(50,150,25);
   fill("#e4d96f");
   
-  //farmer
-    circle (50,130,15)
+  //body
+  circle (50,130,15);
   ellipse (50,140,30,15);
   fill(sk);
-  rect(40,160,20,30)
+  rect(40,160,20,30);
+
   //eyes
   push();
   //left
-  stroke(95, 76, 45)
-  fill(111, 96, 51)
-  circle(48, 150.5, 3)
-  //point(55,150)
+  stroke(95, 76, 45);
+  fill(111, 96, 51);
+  circle(48, 150.5, 3);
   //right
-  circle(58.5, 149.5, 3)
-  //point(45,150)
+  circle(58.5, 149.5, 3);
   pop();
   
   //mouth
@@ -124,17 +97,17 @@ push();
   arc(53.5, 152, 2, 3, 2*PI, PI)
   pop();
   
-//arms 
+  //arms 
   rect(35,160,10,15);
   rect(55,160,10,15);
   
   //feet
-  fill("black")
-  ellipse(45,190,5,10)
-  ellipse(55,190,5,10)
+  fill("black");
+  ellipse(45,190,5,10);
+  ellipse(55,190,5,10);
   
   //overalls 
-  stroke(2)
+  stroke(2);
   fill("#0c426d");
   rect(40,160,5,10);
   rect(55,160,5,10);
@@ -145,11 +118,44 @@ push();
 
   
 
+// function drawBackground(){
+//   background("#5a6c2b");
+//   fill("#8bae4f");
+//   noStroke();
+
+//   square(0,0,100,0);
+//   square(100,100,100);
+//   square(200,200,100);
+//   square(300,300,100);
+//   square(0,200,100);
+//   square(100,300,100);
+//   square(200,0,100);
+//   square(300,100,100);
+
+//   square(0+400,0,100,0);
+//   square(100+400,100,100);
+//   square(200+400,200,100);
+//   square(300+400,300,100);
+//   square(0+400,200,100);
+//   square(100+400,300,100);
+//   square(200+400,0,100);
+//   square(300+400,100,100);
+
+//   square(0,0+400,100,0);
+//   square(100,100+400,100);
+//   square(200,200+400,100);
+//   square(300,300+400,100);
+//   square(0,200+400,100);
+//   square(100,300+400,100);
+//   square(200,0+400,100);
+//   square(300,100+400,100);
   
-  
-
-
-
-
-  
-
+//   square(0+400,0+400,100,0);
+//   square(100+400,100+400,100);
+//   square(200+400,200+400,100);
+//   square(300+400,300+400,100);
+//   square(0+400,200+400,100);
+//   square(100+400,300+400,100);
+//   square(200+400,0+400,100);
+//   square(300+400,100+400,100);
+//   }
