728bf1ee7277b3e31238f174fc895fd92e96fab8
g3hebert <grhe7225@colorado.edu>
2024-12-06 11:10:09 -0700

    new farmer

---
+++ final project.js
@@ -0,0 +1,123 @@
+let c = ("#a34749")
+let sk = ("#dec58f")
+let farmerx = 0
+let farmery = 0
+
+function setup() {
+  createCanvas(400, 400);
+  
+
+  
+}
+
+function keyPressed () {
+  if (keyCode === DOWN_ARROW){farmery +=1}
+
+  if (keyCode === UP_ARROW){farmery -=1}
+
+if (keyCode === LEFT_ARROW){farmerx -=1}
+
+  if (keyCode === RIGHT_ARROW){farmerx = 1}
+  
+}
+
+
+
+
+
+
+function draw() {
+  background(220);
+  
+  
+//grass
+  background("seagreen")
+  fill("darkgreen")
+  noStroke();
+  square(0,0,100,0);
+  square (100,100,100);
+  square(200,200,100);
+  square (300,300,100);
+  square(0,200,100);
+  square(100,300,100);
+  square(200,0,100);
+  square(300,100,100);
+  
+  //flower
+  
+  fill(c);
+  ellipse(50,30,10,20);
+  ellipse(50,60,10,20);
+  ellipse(35,45,20,10);
+  ellipse(65,45,20,10);
+  fill("brown");
+  circle(50,45,15);
+  
+  DrawFarmer()
+   
+}
+
+function DrawFarmer () {
+  
+push ()
+  translate( farmerx * 100, farmery * 100)
+  
+  stroke(1)
+  fill(sk);
+  //halment of farmer
+  let halmet = circle(50,150,25);
+  fill("#e4d96f");
+  
+  //farmer
+    circle (50,130,15)
+  ellipse (50,140,30,15);
+  fill(sk);
+  rect(40,160,20,30)
+  //eyes
+  push();
+  //left
+  stroke(95, 76, 45)
+  fill(111, 96, 51)
+  circle(48, 150.5, 3)
+  //point(55,150)
+  //right
+  circle(58.5, 149.5, 3)
+  //point(45,150)
+  pop();
+  
+  //mouth
+  push();
+  stroke(136, 114, 63);
+  arc(53.5, 152, 2, 3, 2*PI, PI)
+  pop();
+  
+//arms 
+  rect(35,160,10,15);
+  rect(55,160,10,15);
+  
+  //feet
+  fill("black")
+  ellipse(45,190,5,10)
+  ellipse(55,190,5,10)
+  
+  //overalls 
+  stroke(2)
+  fill("#0c426d");
+  rect(40,160,5,10);
+  rect(55,160,5,10);
+  rect(40,170,20,20);
+  
+  pop()
+  
+}
+
+  
+
+  
+  
+
+
+
+
+  
+
